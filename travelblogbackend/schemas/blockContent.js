438af3b9e03eb16b26f308387098ae8c524e36fc
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-04-24 22:51:46 +0200

    blockContent schema

---
+++ travelblogbackend/schemas/blockContent.js
@@ -0,0 +1,16 @@
+export default {
+    name: 'blockContent',
+    title: 'Block Content',
+    type: 'array',
+    of: [
+        {
+            type: 'block',
+            title: 'Block',
+            style: [
+                {
+                    title: 'Normal',
+                    value: 'normal'
+                }
+            ]
+
+        }
