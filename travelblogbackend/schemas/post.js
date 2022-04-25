31980482ea9ec3b3dea99d495ccaf85326e9b0dd
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-04-25 20:00:48 +0200

    update

--- travelblogbackend/schemas/post.js
+++ travelblogbackend/schemas/post.js
@@ -14,62 +14,67 @@ export default {
     type: 'document',
     initialValue: async () => ({
         postedAt: await getPosition()
-            .then(({ coords })=> {
-    return {
-        const { latitude, longitude, altitude } = coords;
-    }
-})
-    })
+            .then(({ coords }) => {
+                const { latitude, longitude, altitude } = coords;
 
-fields: [
-    {
-        name: 'postedAt',
-        title: 'Location',
-        type: 'geopoint',
-    },
-    {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-    },
-    {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-            source: 'title',
-            maxLength: 96,
+                return {
+                    _type: 'geopoint',
+                    lat: latitude,
+                    lng: longitude,
+                    alt: altitude || undefined
+                }
+            })
+            .catch(() => undefined)
+    }),
+    fields: [
+        {
+            name: 'postedAt',
+            title: 'Location',
+            type: 'geopoint',
         },
-    },
-    {
-        name: 'author',
-        title: 'Author',
-        type: 'reference',
-        to: [{ type: 'author' }],
-    },
-    {
-        name: 'mainImage',
-        title: 'Main image',
-        type: 'image',
-        options: {
-            hotspot: true,
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
         },
-    },
-    {
-        name: 'categories',
-        title: 'Categories',
-        type: 'array',
-        of: [{ type: 'reference', to: [{ type: 'category' }] }],
-    },
-    {
-        name: 'publishedAt',
-        title: 'Published At',
-        type: 'datetime',
-    },
-    {
-        name: 'body',
-        title: 'Body',
-        type: 'blockContent',
-    }
-]
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: [{ type: 'author' }],
+        },
+        {
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'category' }] }],
+        },
+        {
+            name: 'publishedAt',
+            title: 'Published At',
+            type: 'datetime',
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+        }
+    ]
 };
